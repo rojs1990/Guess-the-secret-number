f7460b04250bace7f77f7c6951ee0c3355a1783f
rojs1990 <rojs922@gmail.com>
2020-10-18 10:45:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Dela")
+
+let answerText = document.getElementById("answer");
+let secret = Math.floor((Math.random() * 100) + 1);
+console.log(secret)
+
+function checkAnswer() {
+ let answer = parseInt(document.getElementById("userInput").value);
+
+  if(answer === secret) {
+        answerText.textContent = "Correct! ";
+    } else if(answer > secret) {
+        answerText.textContent = "Your guess is TOO HIGH! Try something lower.";
+    } else if(answer < secret) {
+        answerText.textContent = "Your guess is TOO LOW! Try something higher.";
+    }
+}
+
+
+
+let answerButton = document.getElementById("guess")
+answerButton.addEventListener("click", checkAnswer)
